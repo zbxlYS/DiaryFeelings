f0fcc7d362ca95eb9d0ced196e22876abdfe3cee
Hyun jin <blackduvet52@gmail.com>
2023-11-13 05:10:24 +0900

    마이페이지 클릭시 모달창 생성

--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -13,8 +13,14 @@ const Nav: React.FC<SearchComponentProps> = () => {
   // 로그인시 네비 상단바 변경
   const { data: session } = useSession();
   const [isLogin, SetIsLogin] = useState(false);
+  // 마이페이지 모달창
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const { systemTheme, theme, setTheme } = useTheme();
   const currentTheme = theme === "system" ? systemTheme : theme;
+
+  const handleButtonClick = () => {
+    setIsModalOpen(!isModalOpen);
+  };
 
   useEffect(() => {
     // 초기 렌더링 시 테마를 라이트 모드로 설정
@@ -32,43 +38,96 @@ const Nav: React.FC<SearchComponentProps> = () => {
   }, [session]);
 
   return (
-    <div className="">
+    <div className="flex justify-center items-center">
       <nav className="w-[100%] h-[65px] relative ">
         <div className="w-[100%] h-[66px] left-0 top-0 absolute bg-white dark:bg-[#24272F] border-b border-slate-200 dark:border-b-0" />
-        {/* 회원가입 페이지 버튼  */}
-
+        {/* 로그인 회원가입 버튼  */}
         {!isLogin ? (
           <>
             {" "}
             <Link href="/join">
               <button>
                 <div className="w-[110px] h-[25px] right-[1rem] top-[23px] absolute text-center text-black dark:text-[#F6F7F9] text-base font-normal font-['Pretendard']">
-                  회원가입
+                  <span className="text-slate-500 hover:text-slate-900">
+                    회원가입
+                  </span>
                 </div>
               </button>
             </Link>
             <Link href="/api/auth/signin">
               <button>
-                <div className="right-[7.5rem] top-[23px] absolute text-center text-black dark:text-white text-base font-normal">
-                  로그인{" "}
+                <div className="right-[7.5rem] top-[23px] absolute text-center text-black dark:text-white text-base font-normal ">
+                  <span className="text-slate-500 hover:text-slate-900">
+                    로그인
+                  </span>
                 </div>
               </button>
             </Link>
           </>
         ) : (
-          <button className="w-10 h-10 right-[1.5rem] top-[14.5px] absolute border rounded-full">
-            <Image
-              src="/mypage.svg"
-              alt="Mypage Logo"
-              className="ml-[4px] opacity-80 hover:opacity-80 transition duration-300 "
-              width={30}
-              height={50}
-              priority
-            />
-          </button>
-        )}
-        {/* 버튼 클릭시 회원가입 페이지로 전환  */}
+          // 로그인 성공시 마이페이지 아이콘 생성
+          <>
+            <button
+              className="w-10 h-10 right-[1.5rem] top-[14.5px] absolute border rounded-full"
+              onClick={handleButtonClick}
+            >
+              <Image
+                src="/mypage.svg"
+                alt="Mypage Logo"
+                className="ml-[4px] opacity-80 hover:opacity-80 transition duration-300 "
+                width={30}
+                height={30}
+                priority
+              />
+            </button>
 
+            {isModalOpen && (
+              // 모달 컴포넌트 렌더링 컴포넌트로 뺼껀지??
+              <>
+                {/* Overlay */}
+                <div
+                  className="fixed inset-0 bg-black opacity-10 z-40"
+                  onClick={handleButtonClick} // Close the modal on overlay click
+                ></div>
+
+                {/* Modal */}
+                <div
+                  className={`fixed w-[20rem] h-[30rem]top-0 right-0 p-4 bg-white border shadow-md z-50 rounded-l-xl animate-slidein 
+                  `}
+                >
+                  {/* Modal content */}
+                  <div className="animate-none">
+                    {/* Modal content with no animation */}
+                    <p>내정보 </p>
+                    <p>내정보 </p>
+                    <p>내정보 </p>
+                    <p>내정보 </p>
+                  </div>
+                </div>
+              </>
+            )}
+          </>
+        )}
+        {/* 로그인시 일기쓰기 버튼생성  */}
+
+        {isLogin ? (
+          <>
+            <Link href="/" className="absolute right-[11rem] top-[22px] ">
+              <span className="text-slate-500 hover:text-slate-900">
+                일기쓰기
+              </span>
+            </Link>
+            <Link href="/" className="absolute right-[16.5rem] top-[22px]">
+              <span className="text-slate-500 hover:text-slate-900">기록</span>
+            </Link>
+
+            <div className="absolute right-[9rem] top-5 h-7 border-inherit border-r"></div>
+          </>
+        ) : (
+          ""
+        )}
+
+        {/* 다크모드 변경모드  */}
         <button
           type="button"
           className={
@@ -95,7 +154,7 @@ const Nav: React.FC<SearchComponentProps> = () => {
             <Image
               src="/dark.svg"
               alt="Dark Logo"
-              className="w-[35px] opacity-70 hover:opacity-60 transition duration-300"
+              className="w-[35px] pl-[5px] opacity-70 hover:opacity-60 transition duration-300"
               width={50}
               height={40}
               priority
@@ -103,16 +162,14 @@ const Nav: React.FC<SearchComponentProps> = () => {
           )}
         </button>
 
-        {/* 로그인 버튼  */}
-        {/* 버튼 클릭시 로그인페이지로 전환  */}
-
-        <div className="flex justify-center items-center self-center w-[40%] max-w-2xl h-[37px] left-[9rem] bottom-[0.7rem] absolute border-y-2 rounded-full hover:border-gray-300 focus-within:border-gray-300  dark:border-[#9BA3AF]">
+        {/* 검색창  */}
+        <div className="flex justify-center items-center self-center w-[40%] max-w-2xl h-[37px] left-[9rem] bottom-[0.7rem] absolute border border-inherit rounded-full hover:border-gray-300 focus-within:border-gray-300  dark:border-[#9BA3AF] ">
           <Image
             src="/search.svg"
             alt="Search Logo"
-            className="left-[0.5rem]  absolute"
+            className="left-[0.5rem] absolute stroke-slate-600"
             width={23}
-            height={40}
+            height={23}
             priority
           />
           <input
