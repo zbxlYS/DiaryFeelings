3cce55e2e48e8b620b522e9be44f542f42a97964
Hyun jin <blackduvet52@gmail.com>
2023-11-13 16:15:46 +0900

    현진/ components 폴더 위치 수정

--- src/app/join/page.tsx
+++ src/app/join/page.tsx
@@ -1,30 +1,34 @@
-import React from 'react'
-import Essential from '../components/join/Essential'
-import Optional from '../components/join/Optional'
-import Link from 'next/link'
-
+import React from "react";
+import Essential from "./_components/Essential";
+import Optional from "./_components/Optional";
+import Link from "next/link";
 
 const page = () => {
-
-  
   return (
-    <body style={{height:'100vh', display: 'flex', flexDirection:'column'}} >
-    <div style={{display: "flex", justifyContent:'center', alignItems:'center'}}>
-       <div style={{border: "1px dotted black", width: "50%", height:"540px"}}>
-          
+    <body style={{ height: "100vh", display: "flex", flexDirection: "column" }}>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <div
+          style={{ border: "1px dotted black", width: "50%", height: "540px" }}
+        >
           <form action="#">
-            <h1 style={{textAlign: 'center'}}>회원가입</h1>
-            <Essential/>
-            <Optional/>
-            <button type='submit'>가입하기</button>
-            <Link href='/signin'><button>소셜로그인</button></Link>
+            <h1 style={{ textAlign: "center" }}>회원가입</h1>
+            <Essential />
+            <Optional />
+            <button type="submit">가입하기</button>
+            <Link href="/signin">
+              <button>소셜로그인</button>
+            </Link>
           </form>
-
-
-       </div>
-    </div>
+        </div>
+      </div>
     </body>
-  )
-}
+  );
+};
 
-export default page
+export default page;
