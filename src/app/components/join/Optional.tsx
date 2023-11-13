d7dc7e86e8038619d0dbb5b6a27e79d075b96c8e
anna <121926567+anna931124@users.noreply.github.com>
2023-11-13 14:44:12 +0900

    #36 회원가입 내용

---
+++ src/app/components/join/Optional.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Optional = () => {
+  return (
+    <div>
+        <p> 이미지 : <input/> </p>    
+        <p> 닉네임 : <input placeholder='일기장의 이름을 정해주세요'/> </p>    
+    </div>
+  )
+}
+
+export default Optional
