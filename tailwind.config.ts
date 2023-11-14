fe537d81cb05030ac97d77148e549de11ed841e2
Hyun jin <blackduvet52@gmail.com>
2023-11-15 03:50:59 +0900

    tailwind 테마 설정 네비게이션 고정 및 css수정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,24 +1,54 @@
 import type { Config } from 'tailwindcss'
+const { nextui } = require('@nextui-org/react')
 
 const config: Config = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
-  darkMode: 'class',
+  // tailwind
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+      backgroundImage: {},
     },
     fontFamily: {
       pretendard: ['Pretendard-Regular'],
     },
   },
-  plugins: [],
+  darkMode: 'class',
+
+  // next js plugin
+  plugins: [
+    nextui({
+      themes: {
+        light: {
+          colors: {
+            background: '#FFFFFF', // or DEFAULT
+            foreground: '#11181C', // or 50 to 900 DEFAULT
+            primary: {
+              //... 50 to 900
+              foreground: '#FFFFFF',
+              DEFAULT: '#006FEE',
+            },
+            // ... rest of the colors
+          },
+        },
+        dark: {
+          colors: {
+            background: '#171717', // or DEFAULT
+            foreground: '#ECEDEE', // or 50 to 900 DEFAULT
+            primary: {
+              //... 50 to 900
+              foreground: '#FFFFFF',
+              DEFAULT: '#006FEE',
+            },
+          },
+          // ... rest of the colors
+        },
+      },
+    }),
+  ],
 }
 export default config
