39376c1a83743491f8209f31ff6c8c7a8b918efb
김도형 <137859651+ohkjh1@users.noreply.github.com>
2023-11-17 12:43:22 +0900

    #70 DB연결

--- src/app/calendar/page.tsx
+++ src/app/calendar/page.tsx
@@ -1,69 +1,135 @@
 // Page.tsx
-'use client'
-import React, { useState } from 'react'
-import Modal from 'react-modal'
-import Calendar from 'react-calendar'
-import 'react-calendar/dist/Calendar.css'
-import moment from 'moment'
-import { useRouter } from 'next/navigation'
-import './test.css'
+"use client"
+import React, { useState, useEffect } from 'react';
+import Modal from 'react-modal';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+import moment from 'moment';
+import axios from 'axios';
+import { useRouter } from 'next/navigation'; 
+import './test.css';
 
-type ValuePiece = Date | null
-type Value = ValuePiece | [ValuePiece, ValuePiece]
+type ValuePiece = Date | null;
+type Value = ValuePiece | [ValuePiece, ValuePiece];
 
-const ModalCalendar: React.FC<{ isOpen: boolean; closeModal: () => void }> = ({
-  isOpen,
-  closeModal,
-}) => {
-  const [value, onChange] = useState<Value>(new Date())
-  const mark = [
-    '2023-11-24',
-    '2023-12-18',
-    '2024-01-03',
-    '2024-02-04',
-    '2024-04-14',
-    '2024-07-20',
-    '2024-07-28',
-  ]
-  const router = useRouter()
+type ModalCalendarProps = {
+  isOpen: boolean;
+  closeModal: () => void;
+};
 
-  const dayClick = (
-    value: Date,
-    event: React.MouseEvent<HTMLButtonElement, MouseEvent>,
-  ) => {
-    console.log(moment(value).format('YYYY-MM-DD'))
-  }
+interface YourEmotionDataItem {
+  user_id: string;
+  date: string;
+  diary_emotion: string | { [key: string]: string };
+}
+
+const ModalCalendar: React.FC<ModalCalendarProps> = ({ isOpen, closeModal }) => {
+  const [emotionData, setEmotionData] = useState<YourEmotionDataItem[]>([]);
+  const [value, onChange] = useState<Value>(new Date());
+  const router = useRouter();
+
+  const fetchDataFromDatabase = async () => {
+    try {
+      const response = await axios.get<{ result: YourEmotionDataItem[] }>('/api/cal');
+      const { result } = response.data;
+      console.log(result)
+      const emotionDataArray = result || [];
+      console.log('Emotion data from API:', emotionDataArray);
+      setEmotionData(emotionDataArray.map((item: any) => ({
+        ...item,
+        date: moment(item.diary_userDate).format('YYYY-MM-DD'),
+        diary_emotion: typeof item.diary_emotion === 'string' ? JSON.parse(item.diary_emotion) : item.diary_emotion
+      })));
+    } catch (error) {
+      console.error('Error fetching data from the database:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchDataFromDatabase();
+  }, []);
+
+  const dayClick = (value: Date, event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    const formattedDate = moment(value).format('YYYY-MM-DD');
+    router.push(`/write?date=${formattedDate}`);
+    closeModal();
+  };
+
+  const getEmotionImage = (emotionValue: string | { [key: string]: string }): string | null => {
+    if (typeof emotionValue === 'string') {
+      const emotionImages: { [key: string]: string } = {
+        "행복": './happy.png',
+        "분노": './angry.png',
+        "우울": './depress.png',
+        "슬픔": './sad.png',
+        "불안": './nervous.png',
+        "중립": './nothinking.png',
+        "기쁨": './joy.png'
+      };
+  
+      return emotionImages[emotionValue] || null;
+    } else if (typeof emotionValue === 'object') {
+      const key = Object.keys(emotionValue)[0];
+      const emotionImages: { [key: string]: string } = {
+        "행복": './happy.png',
+        "분노": './angry.png',
+        "우울": './depress.png',
+        "슬픔": './sad.png',
+        "불안": './nervous.png',
+        "중립": './nothinking.png',
+        "기쁨": './joy.png'
+      };
+  
+      return emotionImages[key] || null;
+    }
+  
+    return null;
+  };
 
   const handleMarking = (date: Date, view: any) => {
-    const imagesByDate: { [key: string]: string } = {
-      '2023-11-15': './kkomul.png',
-      '2023-11-16': './angry.png',
-      '2023-11-17': './depress.png',
-      '2023-11-25': './sad.png',
-      // 필요에 따라 더 많은 날짜-이미지 매핑 추가
+    const formattedDate = moment(date).format('YYYY-MM-DD');
+    
+    
+    const matchingEmotion = emotionData.find((x) => x.date === formattedDate);
+    
+    if (matchingEmotion) {
+      const emotionValue = matchingEmotion.diary_emotion;
+  
+      if (typeof emotionValue === 'object') {
+        const key = Object.keys(emotionValue)[0];
+        const emotionImages: { [key: string]: string } = {
+          "행복": './happy.png',
+          "분노": './angry.png',
+          "우울": './depress.png',
+          "슬픔": './sad.png',
+        };
+  
+        if (emotionImages[key]) {
+          const html = (
+            <div className='dot' key={formattedDate} onClick={() => handleImageClick(formattedDate)}>
+              <img src={emotionImages[key]} alt={`${formattedDate}의 이미지`} />
+            </div>
+          );
+  
+          return (
+            <div className='dot-img'>
+              {html}
+            </div>
+          );
+        }
+      }
     }
+  
+    return <div className='dot-img'></div>;
+  };
+  
+  
 
-    const html = []
-    const formattedDate = moment(date).format('YYYY-MM-DD')
-
-    if (imagesByDate[formattedDate]) {
-      html.push(
-        <div className="dot" key={formattedDate}>
-          <img
-            src={imagesByDate[formattedDate]}
-            alt={`${formattedDate}의 이미지`}
-            onClick={() => handleImageClick(formattedDate)}
-          />
-        </div>,
-      )
-    }
-
-    return <div className="dot-img">{html}</div>
-  }
   const handleImageClick = (date: string) => {
-    router.push(`/write?date=${date}`)
-    closeModal()
-  }
+    if (date === moment().format('YYYY-MM-DD')) {
+      router.push(`/write?date=${date}`);
+    }
+  };
 
   return (
     <Modal
@@ -96,19 +162,20 @@ const ModalCalendar: React.FC<{ isOpen: boolean; closeModal: () => void }> = ({
         x
       </button>
     </Modal>
-  )
-}
+  );
+};
 
 const Page: React.FC = () => {
-  const [modalIsOpen, setModalIsOpen] = useState(false)
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const router = useRouter();
 
   const openModal = () => {
-    setModalIsOpen(true)
-  }
+    setModalIsOpen(true);
+  };
 
   const closeModal = () => {
-    setModalIsOpen(false)
-  }
+    setModalIsOpen(false);
+  };
 
   return (
     <div>
@@ -118,7 +185,7 @@ const Page: React.FC = () => {
 
       <ModalCalendar isOpen={modalIsOpen} closeModal={closeModal} />
     </div>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
