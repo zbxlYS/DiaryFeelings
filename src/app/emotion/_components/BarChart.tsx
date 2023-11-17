73d9dd832eac661f0bf4ba472232cca42ff9b2db
Hyun jin <blackduvet52@gmail.com>
2023-11-17 14:12:22 +0900

    Summary

--- src/app/emotion/_components/BarChart.tsx
+++ src/app/emotion/_components/BarChart.tsx
@@ -4,7 +4,7 @@ import { Chart, registerables } from 'chart.js'
 // 타입 정의
 type Dataset = {
   label: string
-  data: number[]
+  data: any[]
   backgroundColor: string[]
   borderColor: string[]
   borderWidth: number
@@ -37,26 +37,39 @@ const BarChart = () => {
         // ctx 타입 단언
         type: 'bar',
         data: {
-          labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+          labels: [
+            '행복',
+            '기쁨',
+            '사랑',
+            '화남',
+            '슬픔',
+            '우울',
+            '불안',
+            '생각없음 ',
+          ],
           datasets: [
             {
-              label: '# of Votes',
-              data: [15, 20, 60, 10, 22, 30],
+              label: '{11월} 감정 기록',
+              data: [15, 20, 60, 10, 22, 30, 2, 10],
               backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)',
+                'rgba(255, 159, 64, 0.2)', // 행복
+                'rgba(255, 206, 86, 0.2)', // 기쁨
+                'rgba(239, 203, 207, 0.2)', // 사랑
+                'rgba(255, 99, 132, 0.2)', // 화남
+                'rgba(75, 192, 192, 0.2)', // 슬픔
+                'rgba(174, 221, 251, 0.2)', // 우울
+                'rgba(153, 102, 255, 0.2)', // 불안
+                'rgba(171, 171, 171, 0.2)', // 생각없음
               ],
               borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
                 'rgba(255, 159, 64, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(239, 203, 207, 1)',
+                'rgba(255, 99, 132, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(174, 221, 251, 1)', //우울
+                'rgba(153, 102, 255, 1)', // 불안
+                'rgba(171, 171, 171, 1)', // 생각없음
               ],
               borderWidth: 1,
             },
@@ -64,9 +77,17 @@ const BarChart = () => {
         } as Data, // data 타입 단언
         options: {
           scales: {
+            x: {
+              grid: {
+                display: false, // Set display to false to hide grid lines
+              },
+            },
             y: {
               beginAtZero: true,
               max: 100,
+              grid: {
+                display: false, // Set display to false to hide grid lines
+              },
             },
           },
         } as Options, // options 타입 단언
---
+++ src/app/emotion/_components/MovieCard.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const MovieCard = () => {
+  return (
+    <div>
+      {' '}
+      <div className="w-[17rem] h-[10rem] bg-white mb-10 rounded-md "> </div>
+      <div className="w-[17rem] h-[10rem] bg-white mb-10 rounded-md ml-5">
+        {' '}
+      </div>
+      <div className="w-[17rem] h-[10rem] bg-white mb-10 rounded-md ml-5">
+        {' '}
+      </div>
+      <div className="w-[17rem] h-[10rem] bg-white mb-10 rounded-md ml-5">
+        {' '}
+      </div>
+      <div className="w-[17rem] h-[10rem] bg-white mb-10 rounded-md ml-5">
+        {' '}
+      </div>
+      <div className="w-[17rem] h-[10rem] bg-white mb-10 rounded-md ml-5">
+        {' '}
+      </div>
+      <div className="w-[17rem] h-[10rem] bg-white mb-10 rounded-md ml-5">
+        {' '}
+      </div>
+      <div className="w-[17rem] h-[10rem] bg-white mb-10 rounded-md ml-5">
+        {' '}
+      </div>
+    </div>
+  )
+}
+
+export default MovieCard
