d33057bc5aa78a3da9880c5e463cbf635d95ac34
김도형 <137859651+ohkjh1@users.noreply.github.com>
2023-11-23 14:31:12 +0900

    main 수정 중

--- src/app/components/NotLoginMain.tsx
+++ src/app/components/NotLoginMain.tsx
@@ -1,15 +1,29 @@
 import { useState } from "react";
 
 const NotLoginMain = () => {
-    const [hover, setHover] = useState(null);
+    const [hover, setHover] = useState<string | null>(null);
+    const [lastHoveredImage, setLastHoveredImage] = useState<string | null>(null);
 
-    // 이미지 컴포넌트를 렌더링하는 함수
-    const renderImage = (imageSource) => {
+    const handleMouseOver = (image: string) => {
+        setHover(image);
+        setLastHoveredImage(image); // 마지막으로 호버된 이미지 업데이트
+    };
+
+    const handleMouseOut = () => {
+       
+    };
+
+    const renderImage = (imageSource: string) => {
         return (
             <div className="w-[1400px] h-[300px] border border-[tomato] mt-[50px]">
                 <img src={imageSource} alt="Descriptive Text of Image" className="w-full h-auto object-contain" />
             </div>
         );
+    };
+
+    // 이미지 렌더링 조건을 수정하여 호버된 이미지 또는 마지막으로 호버된 이미지를 표시
+    const shouldRenderImage = (image: string) => {
+        return hover === image || lastHoveredImage === image;
     };
 
     return (
@@ -25,6 +39,7 @@ const NotLoginMain = () => {
                     내 감정을 기록하기
                 </span>
             </div>
+
             <div className="flex flex-col w-full justify-center items-center mt-[100px]">
                 <div className="border w-[1200px] h-[300px] overflow-hidden">
                     <img src="/ddiary.png" alt="Descriptive Text of Image" className="w-full h-auto object-cover object-center" />
@@ -34,8 +49,8 @@ const NotLoginMain = () => {
                     {/* 일기 작성하기 Section */}
                     <div className="flex items-center p-[60px]">
                         <div className={`flex flex-col justify-center items-start p-[20px] rounded-md border cursor-pointer duration-200 ${hover === 'first' ? 'bg-white shadow-2xl' : 'bg-gray-100 shadow-lg'}`}
-                            onMouseOver={() => setHover('first')}
-                            onMouseOut={() => setHover(null)}
+                            onMouseOver={() => handleMouseOver('first')}
+                            onMouseOut={() => handleMouseOut()}
                         >
                             <div className="flex justify-center items-center mb-[15px]">
                                 <div className="w-[25px] h-[25px] border rounded-md bg-[#b2a4d4]"></div>
@@ -47,8 +62,8 @@ const NotLoginMain = () => {
                     {/* 나에게 한 마디 Section */}
                     <div className="flex items-center p-[60px]">
                         <div className={`flex flex-col justify-center items-start p-[20px] rounded-md border cursor-pointer duration-200 ${hover === 'second' ? 'bg-white shadow-2xl' : 'bg-gray-100 shadow-lg'}`}
-                            onMouseOver={() => setHover('second')}
-                            onMouseOut={() => setHover(null)}
+                            onMouseOver={() => handleMouseOver('second')}
+                            onMouseOut={() => handleMouseOut()}
                         >
                             <div className="flex justify-center items-center mb-[15px]">
                                 <div className="w-[25px] h-[25px] border rounded-md bg-[#b2a4d4]"></div>
@@ -60,8 +75,8 @@ const NotLoginMain = () => {
                     {/* 달력 보기 Section */}
                     <div className="flex items-center p-[60px]">
                         <div className={`flex flex-col justify-center items-start p-[20px] rounded-md border cursor-pointer duration-200 ${hover === 'third' ? 'bg-white shadow-2xl' : 'bg-gray-100 shadow-lg'}`}
-                            onMouseOver={() => setHover('third')}
-                            onMouseOut={() => setHover(null)}
+                            onMouseOver={() => handleMouseOver('third')}
+                            onMouseOut={() => handleMouseOut()}
                         >
                             <div className="flex justify-center items-center mb-[15px]">
                                 <div className="w-[25px] h-[25px] border rounded-md bg-[#b2a4d4]"></div>
@@ -73,8 +88,8 @@ const NotLoginMain = () => {
                     {/* 감정 그래프 Section */}
                     <div className="flex items-center p-[60px]">
                         <div className={`flex flex-col justify-center items-start p-[20px] rounded-md border cursor-pointer duration-200 ${hover === 'fourth' ? 'bg-white shadow-2xl' : 'bg-gray-100 shadow-lg'}`}
-                            onMouseOver={() => setHover('fourth')}
-                            onMouseOut={() => setHover(null)}
+                            onMouseOver={() => handleMouseOver('fourth')}
+                            onMouseOut={() => handleMouseOut()}
                         >
                             <div className="flex justify-center items-center mb-[15px]">
                                 <div className="w-[25px] h-[25px] border rounded-md bg-[#b2a4d4]"></div>
@@ -85,14 +100,14 @@ const NotLoginMain = () => {
                 </div>
 
                 {/* 페이지 이용 방법 이미지 Section */}
-                {hover === 'first' && renderImage("/cat.jpg")}
-                {hover === 'second' && renderImage("/catcatcat.png")}
-                {hover === 'third' && renderImage("/달력.png")}
-                {hover === 'fourth' && renderImage("/cat1.png")}
+                {shouldRenderImage('first') && renderImage("/cat.jpg")}
+                {shouldRenderImage('second') && renderImage("/catcatcat.png")}
+                {shouldRenderImage('third') && renderImage("/달력.png")}
+                {shouldRenderImage('fourth') && renderImage("/cat1.png")}
 
                 {/* 나머지 컴포넌트 부분 */}
-                <div className="w-full flex justify-center items-center mt-[200px]">
-                    <span className="mt-[50px] text-[30px] px-[24px] py-[12px] border rounded-md text-white bg-[#b2a4d4] cursor-pointer">
+                <div className="w-full flex justify-center items-center mt-[800px]">
+                    <span className="mt-[50px] text-[30px] px-[24px] py-[12px] border rounded-md text-white bg-[#b2a4d4] cursor-pointer" style={{ marginTop: '100px' }}>
                         감정을 기록하러 가볼까요?
                     </span>
                 </div>
