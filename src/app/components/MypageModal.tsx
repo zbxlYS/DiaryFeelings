7b8dab9a82347c94f6f6061ce95be76e8ffadb4d
PYS_73 <128453499+zbxlYS@users.noreply.github.com>
2024-05-19 16:07:13 +0900

    Update MypageModal.tsx

--- src/app/components/MypageModal.tsx
+++ src/app/components/MypageModal.tsx
@@ -135,6 +135,24 @@ const MypageModal: React.FC<MypageModalProps> = ({
         </Link>
       </div>
 
+
+      <div
+        className={`hover:bg-purple/20 rounded-md p-1 dark:hover:bg-[#666] ${
+          pathname.startsWith(`/follow`) ? 'bg-purple' : ''
+        }`}
+      >
+        <Link
+          href={`/follow`}
+          className="ml-4"
+          onClick={closeModal}
+        >
+          <span className="text-slate-800 hover:text-slate-900 dark:text-[#eee] dark:hover:text-[#b2a4d4]">
+            팔로우 목록
+          </span>
+        </Link>
+      </div>
+
+
       <div className="hover:bg-purple/20 rounded-md p-1 dark:hover:bg-[#666]">
         <Link href="/" className="ml-4" onClick={closeModal}>
           <button onClick={async () => handleSingOut()}>
